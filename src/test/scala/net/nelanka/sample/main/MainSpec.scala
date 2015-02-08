a2576a349b077ec85a423ae663a4951c5d89783b
Nelanka Perera <nelanka@gmail.com>
2015-02-08 13:20:49 -0500

    More gradle script changes. Now runs ScalaTest and Scoverage

--- src/test/scala/net/nelanka/sample/main/MainSpec.scala
+++ src/test/scala/net/nelanka/sample/main/MainSpec.scala
@@ -3,9 +3,7 @@ package net.nelanka.sample.main
 import org.scalatest.{FlatSpec, Matchers}
 
 class MainUnitTest extends FlatSpec with Matchers {
-    behavior of "Main"
-
-    it should "exist" in {
-        1 === 1
-    }
+  "Main" should "exist" in {
+    1 === 1
+  }
 }
